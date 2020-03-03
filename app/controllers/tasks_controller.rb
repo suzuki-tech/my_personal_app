9df43c5900bb7c549e4f77d6fd6a6b008ac01b46
suzuki-tech <nao@store-tech.jp>
2020-03-03 16:12:56 +0900

    ７つのアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,29 @@
 class TasksController < ApplicationController
-
   def index
+    @task = Task.new
+    @tasks = Task.order('created_at ASC')
+    @status = ["start", "doing", "done"]
+  end
+
+  def new
     
   end
-end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  def edit
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+    private
+    def task_params
+      params.require(:task).permit(:content, :status)
+    end
+  end
+
