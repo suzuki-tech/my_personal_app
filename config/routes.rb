4d3d6e280238b1ef2f50f83288fb17b7ddc2a877
suzuki-tech <nao@store-tech.jp>
2020-03-03 16:30:10 +0900

    editのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root "tasks#index"
-  resources :tasks, only: [:index, :new, :create, :destroy]
+  resources :tasks, only: [:index, :new, :create, :destroy, :edit]
 
 end
